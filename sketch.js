f1c1ea306052fb69674fc04dbb2f1c999583088d
anagoncalves2002 <margarisagoncalves@gmail.com>
2023-05-29 20:22:42 +0100

    codigo

--- sketch.js
+++ sketch.js
@@ -1,5 +1,3 @@
-let laranjas;
-
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(0)
@@ -8,10 +6,31 @@ function setup() {
 
 function draw() {
 
+	background(0)
+
+
+if(mouseY >= height/2) {
+	noFill();
+}
+
+else{
+
+	fill(12,108,57); //verde
+}
+
+
+if(mouseX <= width/2) {
+	fill(19,52,146); //azul
+}
+
+if(mouseY < height/2 && mouseX <= width/2) {
+	fill(221,56,56); //vermelho
+
+}
 	stroke(230);
 	strokeWeight(2);
-	noFill();
-	circle(width / 2, height / 2, height / 4);
+	
+	circle(width / 2, height / 2, height / 4); // circulo sempre ao centro do encrã
 
 
 }
